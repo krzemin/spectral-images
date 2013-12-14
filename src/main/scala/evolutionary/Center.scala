f7c903fbfd3c00dff77a91b0f3b80e51b96cf262
nowik <krz.nowicki@gmail.com>
2013-12-14 21:19:48 +0100

    cleanup + for fixes

--- src/main/scala/evolutionary/Center.scala
+++
@@ -1,7 +0,0 @@
-package evolutionary
-
-class Center(isRealC : Boolean, coordinatesC: Array[Double]) extends Point(coordinatesC : Array[Double]){
-	//as read in paper, sometimes we want to hav fake centers, to have different 
-	//cluster numbers in one population
-	val isReal = isRealC
-}
